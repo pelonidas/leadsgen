9a516b37843c878c6fd41bbaff91a43f92f79ed5
Jan Nahalka <janx803y@easv365.dk>
2023-07-11 11:48:17 +0200

    table displayed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-  import "../app.css"
+	import '../app.css';
 </script>
 
-<slot />
+<main class="flex">
+	<aside class="h-[100dvh] w-full max-w-[20%] bg-slate-500">
+		<ul>
+			<li>link1</li>
+			<li>link2</li>
+		</ul>
+	</aside>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
-	import type { PageData } from './$types';
 	import { createSvelteTable, getCoreRowModel, flexRender } from '@tanstack/svelte-table';
 	import type { TableOptions } from '@tanstack/svelte-table';
 	import type { Lead } from '../drizzle/schema';
 
-	export let data: PageData;
+	export let data;
 
 	const options = writable<TableOptions<Lead>>({
 		data: data.leads,
@@ -17,6 +16,35 @@
 			{
 				accessorKey: 'email',
 				header: 'Age'
+			},
+			{
+				accessorKey: 'email',
+				header: 'Email'
+			},
+			{
+				accessorKey: 'phoneNum',
+				header: 'Phone Number'
+			},
+			{
+				accessorKey: 'service',
+				header: 'Service'
+			},
+			{
+				accessorKey: 'status',
+				header: 'Status'
+			},
+			{
+				accessorKey: 'createdAt',
+				accessorFn: (row) => {
+					if (!row.createdAt) return '';
+					const locale: Intl.LocalesArgument = 'sk-SK';
+					const date = new Date(row.createdAt);
+					return `${date.toLocaleDateString(locale)} ${date.toLocaleTimeString(locale, {
+						hour: '2-digit',
+						minute: '2-digit'
+					})}`;
+				},
+				header: 'Created At'
 			}
 		],
 		getCoreRowModel: getCoreRowModel()
@@ -27,11 +55,11 @@
 
 <div class="p-2">
 	<table>
-		<thead>
+		<thead class="border">
 			{#each $table.getHeaderGroups() as headerGroup}
 				<tr>
 					{#each headerGroup.headers as header}
-						<th>
+						<th class="border">
 							{#if !header.isPlaceholder}
 								<svelte:component
 									this={flexRender(header.column.columnDef.header, header.getContext())}
@@ -46,7 +74,7 @@
 			{#each $table.getRowModel().rows as row}
 				<tr>
 					{#each row.getVisibleCells() as cell}
-						<td>
+						<td class="border">
 							<svelte:component this={flexRender(cell.column.columnDef.cell, cell.getContext())} />
 						</td>
 					{/each}
