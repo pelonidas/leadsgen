e5c5fab4bd535371b4eefe3f7b5d1492089b0f19
Jan Nahalka <janx803y@easv365.dk>
2023-07-19 22:40:31 +0200

    prepared for longterm roadmap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,12 @@
 <script>
-	import Sidebar from '$lib/components/sidebar.svelte';
 	import Icon from '@iconify/svelte';
 	import '../app.css';
-	import { isSidebarOpen } from '$lib/stores';
 </script>
 
 <main>
-	<div class="flex h-12 w-full items-center bg-slate-700 text-white">
-		<div class="flex w-full max-w-[20%] items-center px-6">
-			<button on:click={() => ($isSidebarOpen = !$isSidebarOpen)} class="ml-auto">
-				<Icon icon="ph:sidebar-duotone" class="text-3xl" />
-			</button>
-		</div>
-	</div>
+	<div class="flex h-12 w-full items-center bg-slate-700 text-white" />
 	<div class="flex h-[calc(100dvh-3rem)]">
-		<Sidebar />
+		<!-- Sidebar goes here -->
 		<slot />
 	</div>
 </main>
