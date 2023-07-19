0df5aa149ae8b5fbce459b4bcf1127f185acf4c1
Jan Nahalka <janx803y@easv365.dk>
2023-07-19 10:25:01 +0200

    setup schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,33 +1,66 @@
-import type { InferModel } from 'drizzle-orm';
-import { pgTable, pgEnum, uuid, timestamp, text } from 'drizzle-orm/pg-core';
+import { pgTable, text, uuid, timestamp, uniqueIndex, integer } from 'drizzle-orm/pg-core';
 
-export const keyStatus = pgEnum('key_status', ['expired', 'invalid', 'valid', 'default']);
-export const keyType = pgEnum('key_type', [
-	'stream_xchacha20',
-	'secretstream',
-	'secretbox',
-	'kdf',
-	'generichash',
-	'shorthash',
-	'auth',
-	'hmacsha256',
-	'hmacsha512',
-	'aead-det',
-	'aead-ietf'
-]);
-export const factorType = pgEnum('factor_type', ['webauthn', 'totp']);
-export const factorStatus = pgEnum('factor_status', ['verified', 'unverified']);
-export const aalLevel = pgEnum('aal_level', ['aal3', 'aal2', 'aal1']);
-export const codeChallengeMethod = pgEnum('code_challenge_method', ['plain', 's256']);
+import { sql } from 'drizzle-orm';
 
-export const leads = pgTable('leads', {
-	id: uuid('id').defaultRandom().primaryKey().notNull(),
-	createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow(),
-	phoneNum: text('phone_num'),
+export const lead = pgTable('lead', {
+	name: text('name').notNull(),
+	id: uuid('id')
+		.default(sql`uuid_generate_v4()`)
+		.primaryKey()
+		.notNull(),
+	phoneNumber: text('phone_number'),
 	email: text('email'),
 	status: text('status'),
 	service: text('service'),
-	name: text('name').notNull()
+	createdAt: timestamp('created_at', { mode: 'string' }).defaultNow()
 });
 
-export type Lead = InferModel<typeof leads>; // return type when queried
+export const analytics = pgTable(
+	'analytics',
+	{
+		id: uuid('id')
+			.default(sql`uuid_generate_v4()`)
+			.primaryKey()
+			.notNull(),
+		allTrafficSources: text('all_traffic_sources'),
+		browser: text('browser'),
+		city: text('city'),
+		country: text('country'),
+		countryCode: text('country_code'),
+		device: text('device'),
+		fcCampaign: text('fc_campaign'),
+		fcChannel: text('fc_channel'),
+		fcContent: text('fc_content'),
+		fcLanding: text('fc_landing'),
+		fcMedium: text('fc_medium'),
+		fcReferrer: text('fc_referrer'),
+		fcSource: text('fc_source'),
+		fcTerm: text('fc_term'),
+		ipAddress: text('ip_address'),
+		latitude: text('latitude'),
+		lcCampaign: text('lc_campaign'),
+		lcChannel: text('lc_channel'),
+		lcContent: text('lc_content'),
+		lcLanding: text('lc_landing'),
+		lcMedium: text('lc_medium'),
+		lcReferrer: text('lc_referrer'),
+		lcSource: text('lc_source'),
+		lcTerm: text('lc_term'),
+		longtitude: text('longtitude'),
+		os: text('os'),
+		pageVisits: integer('page_visits'),
+		pageVisitedList: text('page_visited_list'),
+		region: text('region'),
+		timePassed: integer('time_passed'),
+		timeZone: text('time_zone'),
+		createdAt: timestamp('created_at', { mode: 'string' }).defaultNow(),
+		leadId: uuid('lead_id')
+			.notNull()
+			.references(() => lead.id, { onDelete: 'cascade' })
+	},
+	(table) => {
+		return {
+			leadIdUniqueContstraint: uniqueIndex('lead_id_unique_contstraint').on(table.leadId)
+		};
+	}
+);
--- src/routes/api/leads/+server.ts
+++ src/routes/api/leads/+server.ts
@@ -1,34 +1,45 @@
 import { db } from '$lib/db';
 import { json } from '@sveltejs/kit';
-import { leads } from '../../../drizzle/schema';
 import type { RequestHandler } from './$types';
 import { z } from 'zod';
+import { analytics, lead } from '../../../drizzle/schema';
 
 const BodySchema = z.object({
 	name: z.string(),
 	email: z.string().email(),
-	phoneNum: z.string(),
+	phoneNumber: z.string(),
 	status: z.string().optional(),
 	service: z.string().optional()
 });
 
-export const GET = (async () => {
-	const allLeads = await db.select().from(leads);
-
-	return json(allLeads);
-}) satisfies RequestHandler;
-
 export const POST = (async ({ request }) => {
 	const body = BodySchema.parse(await request.json());
 
-	const [{ insertedId }] = await db
-		.insert(leads)
-		.values({
-			...body
-		})
-		.returning({
-			insertedId: leads.id
-		});
+	await db.transaction(async () => {
+		const [{ leadId }] = await db
+			.insert(lead)
+			.values({
+				name: body.name,
+				email: body.email,
+				phoneNumber: body.phoneNumber,
+				status: body.status,
+				service: body.service
+			})
+			.returning({
+				leadId: lead.id
+			});
 
-	return json({ insertedId });
+		const res = await db.insert(analytics).values({
+			leadId
+		});
+	});
+
+	// const [{ insertedId }] = await db
+	// 	.insert(lead)
+	// 	.values({
+	// 		name: body.name
+	// 	})
+	// 	.returning({
+	// 		insertedId: lead.id
+	// 	});
 }) satisfies RequestHandler;
