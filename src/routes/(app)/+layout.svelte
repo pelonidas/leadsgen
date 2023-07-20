5cc0affccae2ba23b4f19abd66018c5540020dc4
oliversouc <oliver.souc@hotmail.com>
2023-07-20 16:16:54 +0200

    feat(sidebar): create

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
+	import Sidebar from '$lib/components/sidebar.svelte';
 	import Topbar from '$lib/components/topbar.svelte';
 </script>
 
-<div class="flex h-[100dvh] flex-col px-7 pt-5">
-	<Topbar />
-	<slot />
+<div class="flex justify-between h-[100dvh] px-7 pt-5">
+	<Sidebar/>
+	<div>
+		<Topbar />
+		<slot />
+	</div>
 </div>
